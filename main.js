41fbe605b1cfe8cd49be0536c1f3b8a54af8ba67
Daniela <m.daniela.ocanto@hotmail.com>
2023-07-11 23:55:48 -0300

    entrega1

---
+++ main.js
@@ -0,0 +1,117 @@
+
+
+// EJERCICIO 1 //
+
+/*function comparativa(number) {
+    if (number % 2 === 0) {
+    console.log (number + ' Número Par');
+    }
+    else {
+        console.log (number + ' Número Impar');
+    }
+}
+
+comparativa (7)*/
+
+
+
+
+
+// EJERICIO 2 //
+
+/*function mayorigual(num1, num2) {
+    if (num1 > num2 ) {
+        console.log (num1 + ' es mayor que ' + num2);
+    }
+    else if (num2 > num1) {
+        console.log (num2 + ' es mayor ' + num1);
+    }
+    else {
+        console.log (num1 + ' y ' + num2 + ' son iguales');
+    }
+} 
+
+mayorigual (12, 20);*/
+
+
+
+
+
+// EJERCICIO 3 //
+
+/*function multiplodecinco (num) {
+    if (num % 5 === 0) {
+        console.log (num + ' es múltiplo de 5');
+    }
+    return;
+}
+
+multiplodecinco (40);*/
+
+
+
+
+
+// EJERCICIO 4 //
+
+/*for (let i = 0; i <= 30; i++) {
+    console.log (i)
+}*/
+
+
+
+
+
+// EJERCICIO 5 //
+
+
+/*function palabraNumero (palabra, numero) {
+    for (let i = 0; i < numero; i++) {
+        console.log (palabra, numero);
+    }
+}
+
+palabraNumero ('programacion', 23);*/
+
+
+
+// EJERCICIO 6 // 
+
+/*function imprimirArray (array) {
+    for (let i = 0; i < array.length; i++){
+        console.log(array);
+    }
+}
+
+const array = [5, 4, 3, 2, 1];
+imprimirArray (array);*/
+
+
+
+// EJERCICIO 7 //
+
+
+/*function SerieNumeracion (numeros) {
+    for (let i = 0; i < numeros.length; i++) {
+        console.log (numeros[5]);{
+            continue;
+        }
+    }
+}
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+SerieNumeracion (numeros);*/
+
+
+
+// EJERCICIO 8 //
+
+/*function multiplicar (array, numero) {
+    for (let i = 0; i < array.length; i++) {
+        console.log ()
+    }
+}
+
+const array =  []*/
+
+//INCOMPLETO, ME COSTARON MUCHO Y NO LLEGUE. DEBO REFORZAR MAS.//
